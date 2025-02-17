78ea0c2b48759717efd8b46e69fc7656af0d5193
Kacper Radoszewski <kacper.radoszewski@nordicsemi.no>
2025-02-17 08:37:39 +0100

    sample: clean up the sample and add missing dependencies
    Signed-off-by: Kacper Radoszewski <kacper.radoszewski@nordicsemi.no>

--- sample/src/main.c
+++ sample/src/main.c
@@ -5,26 +5,48 @@
  */
 
 #include <stdio.h>
-#include <zephyr/sys/reboot.h>
+#include <string.h>
+#include <nrf_modem.h>
+#include <sb_fota.h>
 #include <modem/nrf_modem_lib.h>
 #include <modem/lte_lc.h>
-#include <sb_fota.h>
 
-static void modem_fota_callback(enum sb_fota_event e)
+NRF_MODEM_LIB_ON_DFU_RES(sb_fota_dfu_hook, on_modem_lib_dfu, NULL);
+
+static void on_modem_lib_dfu(int32_t dfu_res, void *ctx)
+{
+	ARG_UNUSED(ctx);
+
+	switch (dfu_res) {
+	case NRF_MODEM_DFU_RESULT_OK:
+		printk("Modem firmware update successful.\n");
+		printk("Modem is running the new firmware.\n");
+		break;
+	case NRF_MODEM_DFU_RESULT_HARDWARE_ERROR:
+	case NRF_MODEM_DFU_RESULT_INTERNAL_ERROR:
+		printk("Modem firmware update failed.\n");
+		printk("Fatal error.\n");
+		break;
+	case NRF_MODEM_DFU_RESULT_UUID_ERROR:
+	case NRF_MODEM_DFU_RESULT_AUTH_ERROR:
+	default:
+		printk("Modem firmware update failed.\n");
+		printk("Modem is running non-updated firmware.\n");
+		break;
+	}
+}
+
+static void sb_fota_cb(enum sb_fota_event e)
 {
 	switch(e) {
 	case SB_FOTA_EVENT_DOWNLOADING:
-		printk("Modem FOTA library: Checking for update\n");
+		printk("SoftBank FOTA library: Checking for update\n");
 		break;
 	case SB_FOTA_EVENT_IDLE:
-		printk("Modem FOTA library: Idle\n");
+		printk("SoftBank FOTA library: Idle\n");
 		break;
 	case SB_FOTA_EVENT_MODEM_SHUTDOWN:
-		printk("Modem FOTA library: Update downloaded. Modem will be updated.\n");
-		break;
-	case SB_FOTA_EVENT_REBOOT_PENDING:
-		printk("Modem FOTA library: Modem updated, need to reboot\n");
-		sys_reboot(SYS_REBOOT_COLD);
+		printk("SoftBank FOTA library: Update downloaded. Modem will be updated.\n");
 		break;
 	}
 }
@@ -33,38 +55,16 @@ int main(void)
 {
 	int err;
 
-	printk("Modem FOTA client started\n");
+	printk("SoftBank FOTA client started\n");
 
 	err = nrf_modem_lib_init();
-
-	switch (err) {
-	case NRF_MODEM_DFU_RESULT_OK:
-		printk("Modem firmware update successful!\n");
-		printk("Modem will run the new firmware after reboot\n");
-		sys_reboot(SYS_REBOOT_WARM);
-		break;
-	case NRF_MODEM_DFU_RESULT_UUID_ERROR:
-	case NRF_MODEM_DFU_RESULT_AUTH_ERROR:
-		printk("Modem firmware update failed!\n");
-		printk("Modem will run non-updated firmware on reboot.\n");
-		sys_reboot(SYS_REBOOT_WARM);
-		break;
-	case NRF_MODEM_DFU_RESULT_HARDWARE_ERROR:
-	case NRF_MODEM_DFU_RESULT_INTERNAL_ERROR:
-		printk("Modem firmware update failed!\n");
-		printk("Fatal error.\n");
-		sys_reboot(SYS_REBOOT_WARM);
-		break;
-	case -1:
-		printk("Could not initialize bsdlib.\n");
-		printk("Fatal error.\n");
-		return 1;
-	default:
-		break;
+	if (err) {
+		printk("Failed to initialize the modem library, err %d\n", err);
+		return err;
 	}
 
-	if (sb_fota_init(&modem_fota_callback) != 0) {
-		printk("Failed to initialize modem FOTA\n");
+	if (sb_fota_init(&sb_fota_cb) != 0) {
+		printk("Failed to initialize the SoftBank FOTA library\n");
 		return 1;
 	}
 
